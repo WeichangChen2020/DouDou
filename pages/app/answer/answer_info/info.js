c5d217d8a047cbc7a01ca31791f14687ebc34c14
陈伟昌 <1339849378@qq.com>
2019-01-15 13:10:30 +0800

    增加设置项
    设置自动下一题或手动

--- pages/app/answer/answer_info/info.js
+++ pages/app/answer/answer_info/info.js
@@ -7,40 +7,41 @@ var app = getApp();
 Page({
   data: {
     userType: 'xuechetiku',
-    mid: null,//模拟考ID
-    isShowNewExam: true,//是否显示后台答案统计
-    isNewExam: false,//是否使用后台答案。为true时必须isShowNewExam也为true
-    isLoading: false,//加载
+    mid: null, //模拟考ID
+    isShowNewExam: true, //是否显示后台答案统计
+    isNewExam: false, //是否使用后台答案。为true时必须isShowNewExam也为true
+    isLoading: false, //加载
     swiper: {
       active: 0
     },
     layerlayer: {
-      isLayerShow: false,//默认弹窗
-      layerAnimation: {},//弹窗动画
+      isLayerShow: false, //默认弹窗
+      layerAnimation: {}, //弹窗动画
     },
     storeUrl: '1.0?method=pingshifen.question.collect&course_id=' + wx.getStorageSync('pingshifen_current_course_id'), // 收藏url
-    answerUrl: '1.0?method=pingshifen.question.submit&course_id=' + wx.getStorageSync('pingshifen_current_course_id'),//提交答案URL
+    answerUrl: '1.0?method=pingshifen.question.submit&course_id=' + wx.getStorageSync('pingshifen_current_course_id'), //提交答案URL
     answers: {
-      isShowRemove: false,//是否显示移除按钮
-      onLoadUrl: '1.0?method=pingshifen.question.get_id_items&course_id='+wx.getStorageSync('pingshifen_current_course_id'),//题目号链接    
-      start: 0,//初始题号
-      end: 0,//结束题号
-      allLists: [],//题号数据
-      activeNum: 0,//当前条数
-      showActiveNum: 0,//当前显示条数
-      onceLoadLength: 5,//一次向俩端加载条数
-      url: '1.0?method=pingshifen.question.get_info&course_id=' + wx.getStorageSync('pingshifen_current_course_id'),//题目详情链接
-      isShowTip: false//默认是否显示提示
-    }
+      isShowRemove: false, //是否显示移除按钮
+      onLoadUrl: '1.0?method=pingshifen.question.get_id_items&course_id=' + wx.getStorageSync('pingshifen_current_course_id'), //题目号链接    
+      start: 0, //初始题号
+      end: 0, //结束题号
+      allLists: [], //题号数据
+      activeNum: 0, //当前条数
+      showActiveNum: 0, //当前显示条数
+      onceLoadLength: 5, //一次向俩端加载条数
+      url: '1.0?method=pingshifen.question.get_info&course_id=' + wx.getStorageSync('pingshifen_current_course_id'), //题目详情链接
+      isShowTip: false //默认是否显示提示
+    },
+    autoJump: true, //是否自动跳转到下一题
   },
   //单选逻辑
-  tapRadio: function (e) {
+  tapRadio: function(e) {
     //判断是否为已答题
     if (this.data.answers.allLists[this.data.answers.activeNum].isNoFirst) {
       return false;
     }
     var thisOption = e.currentTarget.dataset.option,
-      list = this.data.answers.allLists[this.data.answers.activeNum].options.map(function (option, i) {
+      list = this.data.answers.allLists[this.data.answers.activeNum].options.map(function(option, i) {
         if (thisOption == option.tip) {
           if (!option.isSelect) {
             // option.isActive = true;
@@ -56,13 +57,13 @@ Page({
     this.tapSelect(e);
   },
   //多选逻辑
-  tapCheckbox: function (e) {
+  tapCheckbox: function(e) {
     //判断是否为已答题
     if (this.data.answers.allLists[this.data.answers.activeNum].isNoFirst) {
       return false;
     }
     var thisOption = e.currentTarget.dataset.option,
-      list = this.data.answers.allLists[this.data.answers.activeNum].options.map(function (option, i) {
+      list = this.data.answers.allLists[this.data.answers.activeNum].options.map(function(option, i) {
         if (thisOption == option.tip) {
           if (!option.isSelect) {
             // option.isActive = true;
@@ -79,27 +80,28 @@ Page({
     this.setData(this.data);
   },
   //答案判断逻辑
-  tapSelect: function (e) {
+  tapSelect: function(e) {
     //判断是否为已答题
     if (this.data.answers.allLists[this.data.answers.activeNum].isNoFirst) {
       return false;
     }
-    var answered = 0, bool = true;
-    this.data.answers.allLists[this.data.answers.activeNum].options.forEach(function (option, i) {
+    var answered = 0,
+      bool = true;
+    this.data.answers.allLists[this.data.answers.activeNum].options.forEach(function(option, i) {
       //解析答案数字编码
       if (option.isSelect) {
         switch (option.tip) {
           case 'A':
-            answered = + 16;
+            answered = +16;
             break;
           case 'B':
-            answered = + 32;
+            answered = +32;
             break;
           case 'C':
-            answered = + 64;
+            answered = +64;
             break;
           case 'D':
-            answered = + 128;
+            answered = +128;
             break;
           default:
             console.log('超出设定');
@@ -147,7 +149,9 @@ Page({
     this.setSwiperList();
     this.setData(this.data);
     //延迟加载滑动  这里暂时设置为手动滑动做下一题
-    if (this.data.answers.activeNum + 1 < this.data.answers.allLists.length) {
+    console.log('this.data.autoJump')
+    console.log(this.data.autoJump)
+    if (this.data.answers.activeNum + 1 < this.data.answers.allLists.length && this.data.autoJump) {
       setTimeout(() => this.onSwiper('left'), 400);
     }
     //传答案
@@ -158,9 +162,9 @@ Page({
       subject: this.data.subject
     })
   },
-  
+
   //页码切换列表效果
-  pageClick: function () {
+  pageClick: function() {
     var layerAnimation = wx.createAnimation({
       transformOrigin: "50% 50%",
       duration: 500,
@@ -177,7 +181,7 @@ Page({
     this.setData(this.data);
   },
   //页码切换列表收缩
-  layerFooterClick: function () {
+  layerFooterClick: function() {
     var layerAnimation = wx.createAnimation({
       transformOrigin: "50% 50%",
       duration: 500,
@@ -190,13 +194,13 @@ Page({
     this.setData(this.data);
   },
   //移除错题逻辑
-  setRemoveList: function (e) {
+  setRemoveList: function(e) {
     var isStore = 0;
     //传答案
     https.setAnswer(this.data.answerUrl, {
       questionID: this.data.answers.allLists[this.data.answers.activeNum].id,
-      answer: 1,//改变为对题
-      choose: this.data.answers.allLists[this.data.answers.activeNum].correctAnswerNum,//改变为正确答案
+      answer: 1, //改变为对题
+      choose: this.data.answers.allLists[this.data.answers.activeNum].correctAnswerNum, //改变为正确答案
       subject: this.data.subject
     }).then((data) => {
       this.initAnswer(() => {
@@ -206,7 +210,7 @@ Page({
             content: '恭喜您已经没有错题了，即将返回首页',
             showCancel: false,
             confirmText: '知道了',
-            success: function (res) {
+            success: function(res) {
               wx.navigateBack(1)
             }
           })
@@ -215,40 +219,40 @@ Page({
     })
   },
   //收藏逻辑
-  collectList: function () {
+  collectList: function() {
     var isStore = 0;
     this.data.answers.allLists[this.data.answers.activeNum].isStore = !this.data.answers.allLists[this.data.answers.activeNum].isStore;
     this.setData(this.data);
     isStore = this.data.answers.allLists[this.data.answers.activeNum].isStore ? 1 : 0;
     https.setStore(this.data.storeUrl, {
-      userType: 'xuechetiku',
-      questionID: this.data.answers.allLists[this.data.answers.activeNum].id,
-      collection: isStore,
-      subject: this.data.subject
-    }).then(data => {
-      if (this.data.type == 'wdsc') {
-        this.initAnswer(() => {
-          if (this.data.answers.allLists.length < 1) {
-            wx.showModal({
-              tip: '提示',
-              content: '您已经没有收藏了，即将返回首页',
-              showCancel: false,
-              confirmText: '知道了',
-              success: function (res) {
-                wx.navigateBack(1)
-              }
-            })
-          }
-        });
-        this.setData(this.data);
-      }
-    })
+        userType: 'xuechetiku',
+        questionID: this.data.answers.allLists[this.data.answers.activeNum].id,
+        collection: isStore,
+        subject: this.data.subject
+      }).then(data => {
+        if (this.data.type == 'wdsc') {
+          this.initAnswer(() => {
+            if (this.data.answers.allLists.length < 1) {
+              wx.showModal({
+                tip: '提示',
+                content: '您已经没有收藏了，即将返回首页',
+                showCancel: false,
+                confirmText: '知道了',
+                success: function(res) {
+                  wx.navigateBack(1)
+                }
+              })
+            }
+          });
+          this.setData(this.data);
+        }
+      })
       .catch(e => {
         this.callBackError(e.message);
       });
   },
   //剔除当前选项操作
-  initAnswer: function (callBack) {
+  initAnswer: function(callBack) {
     this.data.isLoading = false;
     this.setData(this.data);
 
@@ -274,7 +278,7 @@ Page({
 
   },
   //题号变更逻辑
-  setActiveNum: function (e) {
+  setActiveNum: function(e) {
     var thisOption = e.currentTarget.dataset.option - 0;
     this.data.answers.activeNum = thisOption;
     this.data.answers.showActiveNum = thisOption;
@@ -283,17 +287,17 @@ Page({
     this.getSubject();
   },
   //swiper切换
-  setEvent: function (e) {
+  setEvent: function(e) {
     this.data.swiper.touchstartEvent = e;
     return false;
   },
   //滑动结束
-  touchEnd: function (e) {
+  touchEnd: function(e) {
     this.onSwiper(this.getDirection(this.data.swiper.touchstartEvent, e));
     return false;
   },
   //swiper切换
-  onSwiper: function (dire) {
+  onSwiper: function(dire) {
     var that = this,
       active = 0,
       storeSetTime,
@@ -316,7 +320,7 @@ Page({
         delay: 0
       });
 
-    if (!this.$isLock) {//锁屏控制
+    if (!this.$isLock) { //锁屏控制
 
       this.$isLock = true;
 
@@ -338,7 +342,7 @@ Page({
         animationO.translate3d('0', 0, 0).step();
         animationT.translate3d('100%', 0, 0).step();
         if (this.data.answers.activeNum > this.data.answers.start) {
-          active = - 1;
+          active = -1;
         } else {
           this.$isLock = false;
           return;
@@ -360,18 +364,18 @@ Page({
       this.data.swiper.animationS = animationS.export();
       this.data.answers.showActiveNum = this.data.answers.activeNum + active;
       this.setData(this.data);
-      setTimeout(function () {
+      setTimeout(function() {
         that.setHtmlsetHtml(active);
       }, 200);
     }
   },
   //修改页面至正常位置
-  setHtmlsetHtml: function (active) {
+  setHtmlsetHtml: function(active) {
     var animationO = wx.createAnimation({
-      transformOrigin: "50% 50%",
-      duration: 0,
-      delay: 0
-    }),
+        transformOrigin: "50% 50%",
+        duration: 0,
+        delay: 0
+      }),
       animationT = wx.createAnimation({
         transformOrigin: "50% 50%",
         duration: 0,
@@ -404,7 +408,7 @@ Page({
     this.$isLock = false;
   },
   //获得手势方向
-  getDirection: function (startEvent, endEvent) {
+  getDirection: function(startEvent, endEvent) {
     var x = endEvent.changedTouches[0].clientX - startEvent.changedTouches[0].clientX,
       y = endEvent.changedTouches[0].clientY - startEvent.changedTouches[0].clientY,
       pi = 360 * Math.atan(y / x) / (2 * Math.PI);
@@ -422,20 +426,26 @@ Page({
     }
   },
   //切换题目逻辑
-  getSubject: function (callBack) {
-    var that = this, start = this.data.answers.activeNum - this.data.answers.onceLoadLength, end = this.data.answers.activeNum + this.data.answers.onceLoadLength, params;
+  getSubject: function(callBack) {
+    var that = this,
+      start = this.data.answers.activeNum - this.data.answers.onceLoadLength,
+      end = this.data.answers.activeNum + this.data.answers.onceLoadLength,
+      params;
     start = start > 0 ? start : 0;
     end = end + 1 >= this.data.answers.allLists.length ? this.data.answers.allLists.length : end;
     //存放下次展示allallList数据
     params = this.data.answers.allLists.slice(start, end + 1);
     //存放展示allallList数据ID
-    params = params.map(function (data) {
+    params = params.map(function(data) {
       //后台需要int型
       return data.id - 0
     });
-    https.find(this.data.answers.url, { questionID: params, subject: this.data.subject }, {
-      isNewExam: this.data.isShowNewExam && this.data.isNewExam
-    })
+    https.find(this.data.answers.url, {
+        questionID: params,
+        subject: this.data.subject
+      }, {
+        isNewExam: this.data.isShowNewExam && this.data.isNewExam
+      })
       .then(d => {
         console.log(d)
         //注册滑动结束回调
@@ -454,7 +464,7 @@ Page({
       })
   },
   //详情数据加载的回调
-  callBackGetSubject: function (d, start, end) {
+  callBackGetSubject: function(d, start, end) {
     var bool = true;
     d.data.forEach((data, i) => {
       if (!!this.data.answers.allLists[start + i]) {
@@ -489,13 +499,13 @@ Page({
     }
   },
   //错误的回调
-  callBackError: function (e) {
+  callBackError: function(e) {
     wx.showModal({
       title: '错误',
       content: '错误提示是：' + e,
       showCancel: false,
       confirmText: '确认关闭',
-      success: function (res) {
+      success: function(res) {
         if (res.confirm) {
           wx.navigateBack()
         }
@@ -503,32 +513,36 @@ Page({
     })
   },
   onLoad(params) {
-    this.data.subject = params.subject;//科目类别
-    this.data.type = params.type;//科目类别
-    this.data.mid = params.mid || null;//模拟考ID
-    if (params.type == 'wdct' || params.type == 'wdsc') {//判断题型分析是否引用后台答案
+    this.data.subject = params.subject; //科目类别
+    this.data.type = params.type; //科目类别
+    this.data.mid = params.mid || null; //模拟考ID
+    if (params.type == 'wdct' || params.type == 'wdsc') { //判断题型分析是否引用后台答案
       this.data.isShowNewExam = true;
       this.data.isNewExam = true;
     } else if (params.type == 'sjlx') {
       this.data.isShowNewExam = false;
-      wx.setNavigationBarTitle({ title: '自由练习', })
+      wx.setNavigationBarTitle({
+        title: '自由练习',
+      })
     } else if (params.type == 'sxlx') {
-      wx.setNavigationBarTitle({ title: '顺序练习', })
+      wx.setNavigationBarTitle({
+        title: '顺序练习',
+      })
     }
     console.log('params')
     console.log(this.data)
     https.initialize(this.data.answers.onLoadUrl, {
-      subject: params.subject,
-      type: params.type,
-      chapterID: params.chapterID || null,
-      mnksRecordID: this.data.mid
-    }, {
+        subject: params.subject,
+        type: params.type,
+        chapterID: params.chapterID || null,
+        mnksRecordID: this.data.mid
+      }, {
         isNewExam: this.data.isShowNewExam && this.data.isNewExam,
         isShowNewExam: this.data.isShowNewExam
       })
       .then(d => {
         console.log(this.data)
-        if (params.type != 'wdct' && params.type != 'wdsc') {//判断题型分析是否改变初始位置
+        if (params.type != 'wdct' && params.type != 'wdsc') { //判断题型分析是否改变初始位置
           this.data.answers.activeNum = d.activeNum || 0;
           this.data.answers.showActiveNum = d.activeNum || 0;
         }
@@ -548,7 +562,18 @@ Page({
         console.error(e)
       });
   },
-  onUnload() {//页面卸载
+  onUnload() { //页面卸载
 
+  },
+  onShow() { //页面显示
+    let that = this;
+    wx.getStorage({
+      key: 'autoJump',
+      success: function(res) {
+        that.setData({
+          autoJump: res.data
+        })
+      },
+    })
   }
 });
